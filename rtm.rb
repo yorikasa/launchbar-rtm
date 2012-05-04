cd73c3b7d93527ab79229ec28f285d5eacc11f43
yuki <yuki@wter.org>
2012-05-04 23:07:46 +0900

    Streamlined

--- rtm.rb
+++ rtm.rb
@@ -4,10 +4,20 @@ require 'net/http'
 require 'digest/md5'
 require 'rexml/document'
 
+KEY_LOCATION = '/Users/yuki/dev/rtm/keys'
+
 class Parser
 
     def initialize(xml)
         @doc = REXML::Document.new(xml)
+    end
+
+    def status
+        if @doc.root.attributes['stat'] == 'ok'
+            return 1
+        else
+            return nil
+        end
     end
 
     def frob
@@ -52,44 +62,79 @@ class Request
         goto = url + "?" + params.map{|k,v| k + "=" + v}.join("&")
         `open /Applications/Safari.app '#{goto}'`
     end
-    
-    def authenticate()
-        if File.exist?('/Users/yuki/dev/rtm/keys')
-            open('/Users/yuki/dev/rtm/keys', 'r') do |file|
-                @api_key = file.gets.chomp()
-                @shared = file.gets.chomp()
-                @frob = file.gets.chomp()
-                @token = file.gets.chomp()
 
-                params = {'method'=>'rtm.auth.checkToken', 'auth_token'=>@token}
-                resp = request(params)
-                # p resp
-                # 通用しなかったらtoken発行し直し...の手続きをあとで書く
-                return
-            end
-        end
-
-        # 1. Frob
+    def auth_confirm
+        # 1.1 Get Frob
         resp = self.request({'method' => 'rtm.auth.getFrob'})
         parser = Parser.new(resp)
         @frob = parser.frob
-
-        # 2. User Confirmation
-        puts auth_request({"perms" => "delete", "frob" => @frob})
-        puts "Have you done?"
-        while STDIN.getc
-            break
+        open(KEY_LOCATION, 'a') do |file|
+            file.puts
+            file.puts @frob
         end
-        
-        # 3. Token
+
+        # 1.2 User Confirmation
+        puts auth_request({"perms" => "write", "frob" => @frob})
+    end
+
+    def authenticate(frob=nil)
+        # First time, this script needs you to authorize.
+        # So you have to execute the script twice.
+
+        if not frob
+            auth_confirm()
+            return
+        end
+                        
+        # 2 Get Token
         resp = self.request({'method' => 'rtm.auth.getToken', 'frob' => @frob})
         parser = Parser.new(resp)
         @token = parser.token
 
-        open('data.txt', 'w+') do |file|
-            file.puts @frob
+        open(KEY_LOCATION, 'a') do |file|
             file.puts @token
-        end 
+        end
+    end
+    
+    def auth_check()
+        if File.exist?(KEY_LOCATION)
+            keys = []
+            open(KEY_LOCATION, 'r') do |file|
+                keys = file.read.split(/\s+/)
+            end
+            # check if already authenticated
+            if keys.length == 2
+                # assume there's not frob and token (First time)
+                @api_key, @shared = keys
+                authenticate()
+                return nil
+            elsif keys.length == 3
+                # assume there's not token (Second time)
+                @api_key, @shared, @frob = keys
+                authenticate("frob exists")
+            elsif keys.length == 4
+                # Authenticated (Third time and after)
+                @api_key, @shared, @frob, @token = keys
+            else
+                return nil
+            end
+
+            # check if auth_token is valid
+            resp = request({'method'=>'rtm.auth.checkToken'})
+            parser = Parser.new(resp)
+            if not parser.status
+                open(KEY_LOCATION, 'w') do |file|
+                    file.puts @api_key
+                    file.puts @shared
+                end
+                authenticate()
+                return nil
+            end
+            return 'ok'
+        else
+            puts 'You have to first create a file named "keys"'
+            puts 'and put API KEY and Shared Secret.'
+        end
     end
 
     def create_timeline
@@ -98,23 +143,25 @@ class Request
         return parser.timeline
     end
     
-    def add_task
+    def add_task(task_name)
         create_timeline()
         resp = self.request({'method' => 'rtm.tasks.add',
                                 'timeline' => create_timeline,
-                                'name' => ARGV.join(' ').strip(),
+                                'name' => task_name,
                                 'parse' => 1})
     end
     
 end
 
+if ARGV.length > 0
+    task_name = ARGV.join(' ').strip()
+else
+    printf('Task: ')
+    task_name = gets
+end
 
 req = Request.new
-req.authenticate
-req.add_task
+if req.auth_check
+    req.add_task(task_name)
+end
 
-# if ARGV.length > 0
-#     # add to rtm
-# else
-#     # authentication
-# end
